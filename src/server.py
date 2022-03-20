6837c4a3891c69cddea2f01b543f547421b16938
Vegero Vyacheslav <93282939+vvvegero97@users.noreply.github.com>
2022-03-20 13:56:04 +0300

    Update server.py

--- src/server.py
+++ src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "Hello World! This is edited after pipeline!"
+    return "YOU'VE BEEN GNOOOOOOOOOOOOOOOOOOMED!!!"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
